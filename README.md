d5182375ade9d0bd1de1906f182c5c79ed32df79
nikunjnaws <nikunjn@amazon.com>
2016-11-22 15:13:27 -0800

    Update README.md
    Adding the detailed setup and instructions for centralizing ELB logs into CloudWatch Logs
    
    Adding the license file and lambda function

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# cloudwatch-logs-centralize-logs
-A Lambda function that helps in centralizing logs from Elastic Load Balancing (ELB) using Amazon S3 bucket triggers.
+# Cloudwatch Logs Centralize Logs
+
+### Package cloudwatch-logs-centralize-logs
+
+Copyright 2016- Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+## Introduction
+
+It is often useful to centralize log data from different sources as it gets generated. Data can then be used for searching, filtering or doing analysis. The **CloudWatch Logs Centralize Logs** is a Lambda function that helps in centralizing logs from Elastic Load Balancing (ELB) using Amazon S3 bucket triggers. In this lambda function we have showed how ELB logs that are delivered to S3 can be posted to CloudWatch Logs. But it can be modified to read any logs from S3.
+
+## Flow of Events
+
+![Flow of events](https://s3.amazonaws.com/aws-cloudwatch/downloads/cloudwatch-logs-centralize-logs/Demo-1.png)
+
+## Setup Overview
+
+Lambda function is written in Node.js. Since we don't have a specific dependency, that is not in the default libraries in Lambda, a Lambda deployment package is not required. Instead we can use the inline editor in Lambda. You can create a new Lambda function, and copy the code in index.js in this repository to your function.
+
+### Pre-requisite
+
+* S3 bucket where ELB logs can be archived to.
+* Enable archiving of ELB access logs in S3.
+
+### Triggers
+
+* The Lambda fucntion is triggered at an S3 'ObjectCreated' event type
+* You need to also provide the S3 bucket where the ELB logs will be delivered
+
+### Authorization
+
+Since there is a need here for various AWS services making calls to each other, appropriate authorization is required.  This takes the form of configuring an IAM role, to which various authorization policies are attached.  This role will be assumed by the Lambda function when running. The below two permissions are required:
+ 
+1.S3 permits Lambda to fetch the created objects from a given bucket
+
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": "arn:aws:s3:::*"
+        }
+    ]
+}
+```
+
+2.CloudWatch Logs permits Lambda to perform various operations. Below we have given Full Access to CloudWatch logs. You can choose to give specific access to describeLogStreams, describeLogGroups, createLogGroup, createLogStream, putLogEvents
+
+```json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+```
+
+### Lambda Function
+
+***Configurable parameters:***
+
+* Log Group Name: Name of the log group in CloudWatch Logs in which the logs will be published
+* Log Steram Name: Name of the log stream within the log group
+
+***Instructions:***
+
+* Handler: The name of the main code file. In this example we have used index as the name of the handler.
+* The Lambda function reads the data from the S3 object using the S3 getObject API. The data is encoded and compresses.
+* The Lambda function decodes and decompresses the data using the zlib library
+* The data is then send to CloudWatch Logs using the putLogEvents api of CloudWatch Logs
+  * We check for the existence of the specified log group and stream using the describeLogGroups and describeLogStreams. If        not found, we create the group and stream. 
+  * When the log steam is existing, we use the sequenceToken during the putLogEvents call.
+    
+### Lambda Configurtion
+
+This Lambda function was created with runtime Node.js 4.3. It has been tested with 128 KB and 3 second timeout. No VPC was used. You can change the number based on your testing.
+
---
+++ index.js
@@ -0,0 +1,155 @@
+'use strict';
+
+const aws = require('aws-sdk');
+var zlib = require('zlib');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+const cloudWatchLogs = new aws.CloudWatchLogs({
+    apiVersion: '2014-03-28'
+});
+
+//specifying the log group and the log stream name for CloudWatch Logs
+const logGroupName = 'apache-elb-logs' //Name of the log group goes here;
+const logStreamName = 'apache-elb-stream' //Name of the log stream goes here;
+
+exports.handler = (event, context, callback) => {
+
+    // Get the object from the event and show its content type
+    const bucket = event.Records[0].s3.bucket.name;
+    console.log('Name of S3 bucket is:', bucket);
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const params = {
+        Bucket: bucket,
+        Key: key,
+    };
+    s3.getObject(params, (err, data) => {
+        if (err) {
+            console.log(err);
+            const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+            console.log(message);
+            callback(message);
+        } else {
+            //uncompressing the S3 data
+            zlib.gunzip(data.Body, function(error, buffer){
+                if (error) {
+                    console.log('Error uncompressing data', error);
+                    return;
+                }
+                
+                var logData = buffer.toString('ascii');
+                //manage the log group, streams and push log events to CloudWatch Logs
+                manageLogGroups(logData);
+               
+            });
+            callback(null, data.ContentType);
+        }
+    });
+    
+    //Manage the log group
+    function manageLogGroups (logData) {
+        
+        var describeLogGroupParams = {
+            logGroupNamePrefix: logGroupName  
+        };
+        
+        //check if the log group already exists
+        cloudWatchLogs.describeLogGroups(describeLogGroupParams, function (err, data){
+            if (err) {
+                console.log('Error while describing log group:', err);
+                createLogGroup (logData);
+            } else {
+                if (!data.logGroups[0]) {
+                    console.log ('Need to  create log group:', data);
+                    //create log group
+                    createLogGroup(logData);
+                } else {
+                    console.log('Success while describing log group:', data);
+                    manageLogStreams(logData);
+                }
+            }
+        });
+    }
+    
+    //Create log group
+    function createLogGroup (logData) {
+        var logGroupParams = {
+            logGroupName: logGroupName
+        }
+        cloudWatchLogs.createLogGroup(logGroupParams, function (err, data){
+            if (err) {
+                console.log('error while creating log group: ', err, err.stack);
+                return;
+            } else {
+                console.log ('Success in creating log group: ', logGroupName);
+                manageLogStreams(logData);
+            }
+        });
+    }
+    
+    //Manage the log stream and get the sequenceToken
+    function manageLogStreams (logData) {
+        var describeLogStreamsParams = {
+            logGroupName: logGroupName,
+            logStreamNamePrefix: logStreamName 
+        }
+        
+        //check if the log stream already exists and get the sequenceToken
+        cloudWatchLogs.describeLogStreams (describeLogStreamsParams, function (err, data) {
+            if (err) {
+                console.log ('Error during describe log streams:', err);
+                //create log stream
+                createLogStream(logData);
+            } else {
+                if (!data.logStreams[0]) {
+                    console.log ('Need to  create log stream:', data);
+                    //create log stream
+                    createLogStream(logData);
+                } else {
+                    console.log ('Log Stream already defined:', logStreamName);
+                    putLogEvents (data.logStreams[0].uploadSequenceToken, logData);
+                }
+            }
+        });
+    }
+    
+    //Create Log Stream
+    function createLogStream (logData) {
+        var logStreamParams = {
+            logGroupName: logGroupName,
+            logStreamName: logStreamName
+        };
+        
+        cloudWatchLogs.createLogStream(logStreamParams, function (err, data){
+            if (err) {
+                console.log('error while creating log stream: ', err, err.stack);
+                    return;
+            } else {
+                console.log ('Success in creating log stream: ', logStreamName);
+                putLogEvents (null, logData);
+            }
+        });
+    }
+    
+    //Put log events in CloudWatch Logs
+    function putLogEvents (sequenceToken, logData) {
+        var putLogEventParams = {
+            logEvents: [ {
+                message: logData,
+                timestamp: Date.now()
+            }],
+            logGroupName: logGroupName,
+            logStreamName: logStreamName
+        }
+        if (sequenceToken) {
+            putLogEventParams['sequenceToken'] = sequenceToken;
+        }
+        
+        cloudWatchLogs.putLogEvents (putLogEventParams, function (err, data) {
+            if (err) {
+                console.log('Error during put log events: ', err, err.stack);
+                return;
+            } else {
+                console.log('Success in putting log events: ', data);
+            } 
+        });
+    }
+};
