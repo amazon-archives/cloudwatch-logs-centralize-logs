ef8e4972f3cb73c6bb92cb53323ca72216dcf38c
Nikunj Nemani <nikunjn@amazon.com>
2017-02-12 02:58:18 -0800

    Modifying the lambda functions to send each log event in the load balancer as an indovidual log event in CloudWatch Logs. Also setting the timestamp of the log event in CloudWatch Logs as the actual timestamp of the event in Load Balanceer

--- classic-index.js
+++ classic-index.js
@@ -6,6 +6,8 @@ const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 const cloudWatchLogs = new aws.CloudWatchLogs({
     apiVersion: '2014-03-28'
 });
+const readline = require('readline');
+const stream = require('stream');
 
 //specifying the log group and the log stream name for CloudWatch Logs
 const logGroupName = 'classic-elb-logs' //Name of the log group goes here;
@@ -124,27 +126,117 @@ exports.handler = (event, context, callback) => {
         });
     }
     
-    //Put log events in CloudWatch Logs
     function putLogEvents (sequenceToken, logData) {
+        //From http://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_PutLogEvents.html
+        const MAX_BATCH_SIZE = 1048576; // maximum size in bytes of Log Events (with overhead) per invocation of PutLogEvents
+        const MAX_BATCH_COUNT = 10000; // maximum number of Log Events per invocation of PutLogEvents
+        const LOG_EVENT_OVERHEAD = 26; // bytes of overhead per Log Event
+
+        // holds a list of batches
+        var batches = [];
+
+        // holds the list of events in current batch
+        var batch = [];
+
+        // size of events in the current batch
+        var batch_size = 0;
+
+        var bufferStream = new stream.PassThrough();
+        bufferStream.end(logData);
+
+        var rl = readline.createInterface({
+            input: bufferStream
+        });
+
+        var line_count = 0;
+
+        rl.on('line', (line) => {
+            ++line_count;
+
+            var ts = line.split(' ', 1)[0];
+            var tval = Date.parse(ts);
+
+            var event_size = line.length + LOG_EVENT_OVERHEAD;
+
+            batch_size += event_size;
+
+            if(batch_size >= MAX_BATCH_SIZE ||
+                batch.length >= MAX_BATCH_COUNT) {
+                // start a new batch
+                batches.push(batch);
+                batch = [];
+                batch_size = event_size;
+            }
+
+            batch.push({
+                message: line,
+                timestamp: tval
+            });
+        });
+
+        rl.on('close', () => {
+            // add the final batch
+            batches.push(batch);
+            sendBatches(sequenceToken, batches);
+        });
+    }
+    
+    function sendBatches(sequenceToken, batches) {
+        var count = 0;
+        var batch_count = 0;
+
+        function sendNextBatch(err, nextSequenceToken) {
+            if(err) {
+                console.log('Error sending batch: ', err, err.stack);
+                return;
+            } else {
+                var nextBatch = batches.shift();
+                if(nextBatch) {
+                    // send this batch
+                    ++batch_count;
+                    count += nextBatch.length;
+                    sendBatch(nextSequenceToken, nextBatch, sendNextBatch);
+                } else {
+                    // no more batches: we are done
+                    var msg = `Successfully put ${count} events in ${batch_count} batches`;
+                    console.log(msg);
+                    callback(null, msg);
+                }
+            }
+        }
+
+        sendNextBatch(null, sequenceToken);
+    }
+
+    function sendBatch(sequenceToken, batch, doNext) {
         var putLogEventParams = {
-            logEvents: [ {
-                message: logData,
-                timestamp: Date.now()
-            }],
+            logEvents: batch,
             logGroupName: logGroupName,
             logStreamName: logStreamName
         }
         if (sequenceToken) {
             putLogEventParams['sequenceToken'] = sequenceToken;
         }
-        
+
+        // sort the events in ascending order by timestamp as required by PutLogEvents
+        putLogEventParams.logEvents.sort(function(a, b) {
+            if(a.timestamp > b.timestamp) {
+                return 1;
+            }
+            if(a.timestamp < b.timestamp) {
+                return -1;
+            }
+            return 0;
+        });
+
         cloudWatchLogs.putLogEvents (putLogEventParams, function (err, data) {
             if (err) {
                 console.log('Error during put log events: ', err, err.stack);
-                return;
+                doNext(err, null);
             } else {
-                console.log('Success in putting log events: ', data);
-            } 
+                console.log(`Success in putting ${putLogEventParams.logEvents.length} events`);
+                doNext(null, data.nextSequenceToken);
+            }
         });
     }
 };
--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const cloudWatchLogs = new aws.CloudWatchLogs({
     apiVersion: '2014-03-28'
 });
 
+const readline = require('readline');
+const stream = require('stream');
+
 //specifying the log group and the log stream name for CloudWatch Logs
-const logGroupName = 'apache-alb-logs' //Name of the log group goes here;
-const logStreamName = 'apache-alb-stream' //Name of the log stream goes here;
+const logGroupName = 'apache-elb-logs' //Name of the log group goes here;
+const logStreamName = 'apache-elb-stream' //Name of the log stream goes here;
 
 exports.handler = (event, context, callback) => {
 
     // Get the object from the event and show its content type
+    console.log('S3 object is:', event.Records[0].s3);
     const bucket = event.Records[0].s3.bucket.name;
     console.log('Name of S3 bucket is:', bucket);
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
@@ -30,15 +34,15 @@ exports.handler = (event, context, callback) => {
         } else {
             //uncompressing the S3 data
             zlib.gunzip(data.Body, function(error, buffer){
-                if (error) {
-                    console.log('Error uncompressing data', error);
-                    return;
-                }
+            if (error) {
+                console.log('Error uncompressing data', error);
+                return;
+            }
+
+            var logData = buffer.toString('ascii');
+            //manage the log group, streams and push log events to CloudWatch Logs
+            manageLogGroups(logData);            
                 
-                var logData = buffer.toString('ascii');
-                //manage the log group, streams and push log events to CloudWatch Logs
-                manageLogGroups(logData);
-               
             });
             callback(null, data.ContentType);
         }
@@ -129,27 +133,117 @@ exports.handler = (event, context, callback) => {
         });
     }
     
-    //Put log events in CloudWatch Logs
     function putLogEvents (sequenceToken, logData) {
+        //From http://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_PutLogEvents.html
+        const MAX_BATCH_SIZE = 1048576; // maximum size in bytes of Log Events (with overhead) per invocation of PutLogEvents
+        const MAX_BATCH_COUNT = 10000; // maximum number of Log Events per invocation of PutLogEvents
+        const LOG_EVENT_OVERHEAD = 26; // bytes of overhead per Log Event
+
+        // holds a list of batches
+        var batches = [];
+
+        // holds the list of events in current batch
+        var batch = [];
+
+        // size of events in the current batch
+        var batch_size = 0;
+
+        var bufferStream = new stream.PassThrough();
+        bufferStream.end(logData);
+
+        var rl = readline.createInterface({
+            input: bufferStream
+        });
+
+        var line_count = 0;
+
+        rl.on('line', (line) => {
+            ++line_count;
+
+            var ts = line.split(' ', 2)[1];
+            var tval = Date.parse(ts);
+
+            var event_size = line.length + LOG_EVENT_OVERHEAD;
+
+            batch_size += event_size;
+
+            if(batch_size >= MAX_BATCH_SIZE ||
+                batch.length >= MAX_BATCH_COUNT) {
+                // start a new batch
+                batches.push(batch);
+                batch = [];
+                batch_size = event_size;
+            }
+
+            batch.push({
+                message: line,
+                timestamp: tval
+            });
+        });
+
+        rl.on('close', () => {
+            // add the final batch
+            batches.push(batch);
+            sendBatches(sequenceToken, batches);
+        });
+    }
+    
+    function sendBatches(sequenceToken, batches) {
+        var count = 0;
+        var batch_count = 0;
+
+        function sendNextBatch(err, nextSequenceToken) {
+            if(err) {
+                console.log('Error sending batch: ', err, err.stack);
+                return;
+            } else {
+                var nextBatch = batches.shift();
+                if(nextBatch) {
+                    // send this batch
+                    ++batch_count;
+                    count += nextBatch.length;
+                    sendBatch(nextSequenceToken, nextBatch, sendNextBatch);
+                } else {
+                    // no more batches: we are done
+                    var msg = `Successfully put ${count} events in ${batch_count} batches`;
+                    console.log(msg);
+                    callback(null, msg);
+                }
+            }
+        }
+
+        sendNextBatch(null, sequenceToken);
+    }
+
+    function sendBatch(sequenceToken, batch, doNext) {
         var putLogEventParams = {
-            logEvents: [ {
-                message: logData,
-                timestamp: Date.now()
-            }],
+            logEvents: batch,
             logGroupName: logGroupName,
             logStreamName: logStreamName
         }
         if (sequenceToken) {
             putLogEventParams['sequenceToken'] = sequenceToken;
         }
-        
+
+        // sort the events in ascending order by timestamp as required by PutLogEvents
+        putLogEventParams.logEvents.sort(function(a, b) {
+            if(a.timestamp > b.timestamp) {
+                return 1;
+            }
+            if(a.timestamp < b.timestamp) {
+                return -1;
+            }
+            return 0;
+        });
+
         cloudWatchLogs.putLogEvents (putLogEventParams, function (err, data) {
             if (err) {
                 console.log('Error during put log events: ', err, err.stack);
-                return;
+                doNext(err, null);
             } else {
-                console.log('Success in putting log events: ', data);
-            } 
+                console.log(`Success in putting ${putLogEventParams.logEvents.length} events`);
+                doNext(null, data.nextSequenceToken);
+            }
         });
     }
 };
